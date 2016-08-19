c2e0bbc5dd80f9567ea0afbe69a59a56ba205682
Hentie Louw <hentie.louw@gmail.com>
2016-08-20 00:36:31 +0100

    Init webpack version of app.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
