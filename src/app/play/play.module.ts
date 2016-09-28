e558ebd609c245bcc5afe004229efcd7aaf23557
Hentie Louw <hentie.louw@gmail.com>
2016-09-28 12:10:32 +0100

    Removed deleted component refs from play module

--- src/app/play/play.module.ts
+++ src/app/play/play.module.ts
@@ -6,8 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { PlayComponent } from './play.component';
 import { PlayerComponent } from './player';
 import { PlayerActionsComponent } from './player-actions';
-import { DeckComponent } from './deck';
-import { BurnComponent } from './burn';
 import { CardCountComponent } from './card-count';
 import { CardNamePipe } from './shared/card';
 import { routing } from './play.routing';
@@ -16,9 +14,7 @@ import { GameActions, GameService, reducer, GameEffects } from './shared/game';
 @NgModule({
   declarations: [
     PlayComponent,
-    DeckComponent,
     CardCountComponent,
-    BurnComponent,
     PlayerComponent,
     PlayerActionsComponent,
     CardNamePipe
