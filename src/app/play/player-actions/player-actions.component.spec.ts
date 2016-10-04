3cf32f9555653c5fffdbccc5c09c074001d41088
Hentie Louw <hentie.louw@gmail.com>
2016-10-04 10:59:16 +0100

    Fix test

--- src/app/play/player-actions/player-actions.component.spec.ts
+++ src/app/play/player-actions/player-actions.component.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, inject } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 import { PlayerActionsComponent } from './player-actions.component';
 
 describe('Component: PlayerActions', () => {
   it('should create an instance', () => {
-    let component = new PlayerActionsComponent();
+    let fb: FormBuilder;
+    let component = new PlayerActionsComponent(fb);
     expect(component).toBeTruthy();
   });
 });
